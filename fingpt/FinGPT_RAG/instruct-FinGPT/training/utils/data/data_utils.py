a6de075056ab3721be2d396bbd135a6756f49ded
Adamakimoo <114376655+Adamakimoo@users.noreply.github.com>
2025-10-08 21:23:33 +1100

    Fix typos in comment

--- fingpt/FinGPT_RAG/instruct-FinGPT/training/utils/data/data_utils.py
+++ fingpt/FinGPT_RAG/instruct-FinGPT/training/utils/data/data_utils.py
@@ -364,7 +364,7 @@ class DataCollatorRLHF:
                                    padding_value=0,
                                    batch_first=True)
 
-        ### make sure the final ouput is a seqence of 2**?
+        ### make sure the final output is a sequence of 2**?
         length = prompt.size()[-1]
         pad_length = self.max_token_len - length
         if pad_length > 0:
